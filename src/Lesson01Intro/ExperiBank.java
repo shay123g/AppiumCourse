560eaa5931e8bd19eb35395d38f9720d239ecfb2
shay123g <49873692+shay123g@users.noreply.github.com>
2019-07-02 20:19:05 +0300

    +a

--- src/Lesson01Intro/ExperiBank.java
+++ src/Lesson01Intro/ExperiBank.java
@@ -1,5 +1,59 @@
-package Lesson01Intro;
+package Lesson01Intro;//package <set your test package>;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.By;
+import org.testng.annotations.*;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
 
-public class ExperiBank
-{
+public class ExperiBank {
+    private String reportDirectory = "reports";
+    private String reportFormat = "xml";
+    private String testName = "Untitled";
+    protected AndroidDriver<AndroidElement> driver = null;
+
+    DesiredCapabilities dc = new DesiredCapabilities();
+
+    @BeforeClass
+    public void setUp() throws MalformedURLException {
+        dc.setCapability("reportDirectory", reportDirectory);
+        dc.setCapability("reportFormat", reportFormat);
+        dc.setCapability("testName", testName);
+        dc.setCapability(MobileCapabilityType.UDID, "ad0117020c03f11a29");
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.experitest.ExperiBank");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".LoginActivity");
+        driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), dc);
+        driver.setLogLevel(Level.INFO);
+    }
+
+    @Test
+    public void testUntitled() {
+        driver.startActivity("com.experitest.ExperiBank", ".LoginActivity");
+        driver.findElement(By.xpath("//*[@id='usernameTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@id='passwordTextField']")).sendKeys("company");
+        driver.findElement(By.xpath("//*[@text='Login']")).click();
+        driver.findElement(By.xpath("//*[@text='Make Payment']")).click();
+        driver.findElement(By.xpath("//*[@id='phoneTextField']")).sendKeys("123");
+        driver.findElement(By.xpath("//*[@id='nameTextField']")).sendKeys("ac");
+        driver.findElement(By.xpath("//*[@id='amountTextField']")).sendKeys("11");
+        driver.findElement(By.xpath("//*[@id='countryTextField']")).click();
+        driver.findElement(By.xpath("//*[@text='Select']")).click();
+        driver.findElement(By.xpath("//*[@text='New Zealand']")).click();
+        driver.findElement(By.xpath("//*[@text='Send Payment']")).click();
+        driver.findElement(By.xpath("//*[@text='Yes']")).click();
+        driver.findElement(By.xpath("//*[@text='Expense Report']")).click();
+        driver.findElement(By.xpath("//*[@text='Add']")).click();
+        driver.findElement(By.xpath("//*[@text=concat('Press ', \"'\", 'Add', \"'\", ' to add row')]")).click();
+        driver.findElement(By.xpath("//*[@text='Back']")).click();
+        driver.findElement(By.xpath("//*[@text='Logout']")).click();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
+    }
 }
