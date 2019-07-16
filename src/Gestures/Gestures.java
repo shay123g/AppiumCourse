d848672b66ad83142654abdd7fb17717bb4e3183
shay123g <49873692+shay123g@users.noreply.github.com>
2019-07-16 16:33:12 +0300

    +a

--- src/Gestures/Gestures.java
+++ src/Gestures/Gestures.java
@@ -4,6 +4,7 @@ package Gestures;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidKeyCode;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
@@ -51,7 +52,26 @@ public class Gestures
          }
     }
 
+    @Test
+    public void Test02_Menu()
+    {
 
+        driver.pressKeyCode(AndroidKeyCode.BACK);
+        driver.pressKeyCode(AndroidKeyCode.BACK);
+        action=new TouchAction(driver);
+        driver.performTouchAction(action.tap(driver.findElement(By.xpath("//*[@text='Expandable Lists']"))));
+        action=new TouchAction(driver);
+        driver.performTouchAction(action.tap(driver.findElement(By.xpath("//*[@text='1. Custom Adapter']"))));
+        AndroidElement longpress=driver.findElement(By.xpath("//*[@text='People Names']"));
+        action=new TouchAction(driver);
+        driver.performTouchAction(action.longPress(longpress));
+       // driver.performTouchAction(action.release());
+       // driver.tap(1,longpress.getRect().getX(),longpress.getRect().getY(),4);
+      //  driver.performTouchAction(action.release());
+
+
+
+    }
     @AfterClass
     public void tearDown()
     {
